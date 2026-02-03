6464105ea973a9583bd37a5266a097cc9408c619
drldx <168914904+drldx@users.noreply.github.com>
2026-02-03 13:34:58 +0530

    add: caesarCipher test

--- samples/caesarCipher.js
+++ samples/caesarCipher.js
@@ -1,12 +1,50 @@
-//export {caesarCipher} 
+export {caesarCipher} 
 
 function caesarCipher(msg, num) {
-  const msgArr = msg.split('');
-  const resArr = [];
 
-  for(msg of msgArr) {
-    console.log(msg + 3); 
+  const alphabetArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const cloneArr = [...alphabetArr];
+  //const upperCaseArr = alphabetArr.map(item => item.toUpperCase());
+  const upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  const upperClone = [...upperArr];
+
+  function swap(num) {
+    
+    let left = 0;
+    let right = 25;
+
+    for(let i = 0; i < num; i++){
+
+      const currLetter = cloneArr.shift();
+      cloneArr.push(currLetter);
+
+      const currL = upperClone.shift();
+      upperClone.push(currL);
+    }
   }
+
+  swap(num);
+
+  function getCipher(string) {
+
+    const data = string.split("");
+    let cipherData = [];
+
+    for(i of data) {
+
+      if(alphabetArr.includes(i)) {
+        let index = alphabetArr.indexOf(i);
+        cipherData.push(cloneArr[index]);    
+      } else if(upperArr.includes(i)){
+        let index = upperArr.indexOf(i);
+        cipherData.push(upperClone[index]);
+      } else {
+        cipherData.push(i);
+      }
+    }
+    return cipherData.join("");
+  }
+   
+  return cipherText = getCipher(msg);
 }
 
-caesarCipher("xyz", 3);
--- samples/caesarCipher.test.js
+++ samples/caesarCipher.test.js
@@ -3,3 +3,11 @@ import {caesarCipher} from './caesarCipher.js';
 it('cipher 3', () => {
   expect(caesarCipher('xyz', 3)).toEqual('abc');
 })
+
+test('case preservation', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+})
+
+test('punctuation test', () => {
+  expect(caesarCipher('Hello, World!', 3)).toEqual('Khoor, Zruog!');
+})
